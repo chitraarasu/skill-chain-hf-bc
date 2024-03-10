503771e1b34463745a969fae70365da0a689c77c
Chitraarasu <98887175+chitraarasu-kirshi@users.noreply.github.com>
2024-03-10 18:47:55 +0530

    api completed

--- skill-chain/skill-chain-chaincode/lib/assetTransfer.js
+++ skill-chain/skill-chain-chaincode/lib/assetTransfer.js
@@ -1,183 +1,216 @@
-'use strict';
+"use strict";
 
-const stringify  = require('json-stringify-deterministic');
-const sortKeysRecursive  = require('sort-keys-recursive');
-const { Contract } = require('fabric-contract-api');
+const stringify = require("json-stringify-deterministic");
+const sortKeysRecursive = require("sort-keys-recursive");
+const { Contract } = require("fabric-contract-api");
 
 class AssetTransfer extends Contract {
+  /// Create New User
+  async CreateUser(
+    ctx,
+    name,
+    address,
+    imageUrl,
+    mobileNo,
+    emailId,
+    publicId,
+    status,
+    password,
+    createdAt
+  ) {
+    const user = {
+      name: name,
+      address: address,
+      imageUrl: imageUrl,
+      mobile_no: mobileNo,
+      email_id: emailId,
+      public_id: publicId,
+      status: true,
+      password: password,
+      skills: [],
+      created_at: createdAt,
+    };
 
-    /// Create New User
-    async CreateUser(ctx, name, address, imageUrl, mobileNo, emailId, publicId, status, password, createdAt) {
-    
-        const user = {
-            name: name,
-            address: address,
-            imageUrl: imageUrl,
-            mobile_no: mobileNo,
-            email_id: emailId,
-            public_id: publicId,
-            status: true,
-            password: password,
-            skills: [],
-            created_at: createdAt
-        }; 
-         
-        const exists = await this.CheckEmailExist(ctx, user.email_id);
-        if (exists) {
-            throw new Error(`The asset email_id already exists`);
+    const exists = await this.CheckEmailExist(ctx, user.email_id);
+    if (exists) {
+      throw new Error(`The asset email_id already exists`);
+    }
+    try {
+      await ctx.stub.putState(
+        publicId,
+        Buffer.from(stringify(sortKeysRecursive(user)))
+      );
+    } catch (e) {
+      console.log(e);
+    }
+    return JSON.stringify(user);
+  }
+
+  /// Login user using publicid and password
+  async ValidateUser(ctx, publicId, password) {
+    const exists = await this.CheckPublicIdPasswordExist(
+      ctx,
+      publicId,
+      password
+    );
+    if (exists == null) {
+      throw new Error(`Invalid user details!`);
+    }
+    return exists;
+  }
+
+  // Use this to
+  // 1. Get user by public ids
+  async GetAllUsers(ctx) {
+    const iterator = await ctx.stub.getStateByRange("", "");
+    let results = await this._GetAllResults(iterator, false);
+    return JSON.stringify(results);
+  }
+
+  // Add skill to user
+  async AddSkill(ctx, userId, skillId, skillDocUrl, instituteId) {
+    const exists = await this.GetUserById(ctx, userId);
+    var matchedData;
+    exists.skills.forEach((element) => {
+      if (element.skill_id === skillId) {
+        matchedData = element;
+      }
+    });
+    if (matchedData != null) {
+      throw new Error(`Skill already added.`);
+    }
+    let skill = {
+      skill_id: skillId,
+      skill_doc: skillDocUrl,
+      institute_id: instituteId,
+    };
+    exists.skills.push(skill);
+    await ctx.stub.putState(
+      userId,
+      Buffer.from(stringify(sortKeysRecursive(exists)))
+    );
+    return exists;
+  }
+
+  // // DeleteAsset deletes an given asset from the world state.
+  // async DeleteAsset(ctx, id) {
+  //     const exists = await this.CheckUserExistById(ctx, id);
+  //     if (!exists) {
+  //         throw new Error(`The asset ${id} does not exist`);
+  //     }
+  //     return ctx.stub.deleteState(id);
+  // }
+
+  // // TransferAsset updates the owner field of asset with given id in the world state.
+  // async TransferAsset(ctx, id, newOwner) {
+  //     const assetString = await this.GetUserById(ctx, id);
+  //     const asset = JSON.parse(assetString);
+  //     const oldOwner = asset.Owner;
+  //     asset.Owner = newOwner;
+  //     // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
+  //     await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
+  //     return oldOwner;
+  // }
+
+  /// Validation
+
+  async CheckEmailExist(ctx, email) {
+    let queryString = {};
+    queryString.selector = {};
+    queryString.selector.email_id = email;
+    const assetJSON = await this.GetQueryResultForQueryString(
+      ctx,
+      JSON.stringify(queryString)
+    );
+    return assetJSON && assetJSON.length > 0;
+  }
+
+  async CheckPublicIdPasswordExist(ctx, publicId, password) {
+    let queryString = {};
+    queryString.selector = {};
+    queryString.selector.public_id = publicId;
+    queryString.selector.password = password;
+    const assetJSON = await this.GetQueryResultForQueryString(
+      ctx,
+      JSON.stringify(queryString)
+    );
+    return assetJSON;
+  }
+
+  /// Query
+
+  async GetQueryResultForQueryString(ctx, queryString) {
+    let resultsIterator = await ctx.stub.getQueryResult(queryString);
+    let results = await this._GetAllResults(resultsIterator, false);
+    return results;
+  }
+
+  async GetUserById(ctx, id) {
+    const assetJSON = await ctx.stub.getState(id);
+    if (!assetJSON || assetJSON.length === 0) {
+      throw new Error(`The asset ${id} does not exist`);
+    }
+    return JSON.parse(assetJSON.toString());
+  }
+
+  /// Utils
+
+  async _GetAllResults(iterator, isHistory) {
+    let allResults = [];
+    let res = await iterator.next();
+    while (!res.done) {
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+        console.log(res.value.value.toString("utf8"));
+        if (isHistory && isHistory === true) {
+          jsonRes.TxId = res.value.txId;
+          jsonRes.Timestamp = res.value.timestamp;
+          try {
+            jsonRes.Value = JSON.parse(res.value.value.toString("utf8"));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Value = res.value.value.toString("utf8");
+          }
+        } else {
+          jsonRes.Key = res.value.key;
+          try {
+            jsonRes.Record = JSON.parse(res.value.value.toString("utf8"));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Record = res.value.value.toString("utf8");
+          }
         }
-        try {
-          await ctx.stub.putState(publicId, Buffer.from(stringify(sortKeysRecursive(user))));  
-        } catch (e){
-            console.log(e);
-        }
-        return JSON.stringify(user);
+        allResults.push(jsonRes);
+      }
+      res = await iterator.next();
     }
+    iterator.close();
+    return allResults;
+  }
 
-    /// Login user using publicid and password
-    async ValidateUser(ctx, publicId, password) {
-        const exists = await this.CheckPublicIdPasswordExist(ctx, publicId, password);
-        if (exists == null) {
-            throw new Error(`Invalid user details!`);
-        }
-        return exists;
+  /// Temp
+  async AddDummyUsers(ctx, count) {
+    try {
+      for (let i = 1; i <= count; i++) {
+        let res = await this.CreateUser(
+          ctx,
+          stringify(i),
+          "user name" + i,
+          "address " + i,
+          "imageUrl_" + i,
+          "mobileNo_" + i,
+          "emailId_" + i,
+          "publicId_" + i,
+          true,
+          "123456",
+          "2023-11-09T10:37:26.031Z"
+        );
+      }
+      return "Success";
+    } catch (error) {
+      return Error(error);
     }
-
-    // Use this to
-    // 1. Get user by public ids
-    async GetAllUsers(ctx) {
-        const iterator = await ctx.stub.getStateByRange('', '');
-        let results = await this._GetAllResults(iterator, false);
-        return JSON.stringify(results);
-    }
-
-    // Add skill to user
-    async AddSkill(ctx, userId, skillId, skillDocUrl, instituteId) {
-        const exists = await this.GetUserById(ctx, userId);
-        var matchedData;
-        exists.skills.forEach(element => {
-            if(element.skill_id === skillId){
-                matchedData = element;
-            }
-        });
-        if(matchedData != null){
-            throw new Error(`Skill already added.`);
-        }
-        let skill = {
-            skill_id: skillId,
-            skill_doc: skillDocUrl,
-            institute_id: instituteId,
-        };
-        exists.skills.push(skill);
-        await ctx.stub.putState(userId, Buffer.from(stringify(sortKeysRecursive(exists))));
-        return exists;
-    }
-
-    // // DeleteAsset deletes an given asset from the world state.
-    // async DeleteAsset(ctx, id) {
-    //     const exists = await this.CheckUserExistById(ctx, id);
-    //     if (!exists) {
-    //         throw new Error(`The asset ${id} does not exist`);
-    //     }
-    //     return ctx.stub.deleteState(id);
-    // }
-
-    // // TransferAsset updates the owner field of asset with given id in the world state.
-    // async TransferAsset(ctx, id, newOwner) {
-    //     const assetString = await this.GetUserById(ctx, id);
-    //     const asset = JSON.parse(assetString);
-    //     const oldOwner = asset.Owner;
-    //     asset.Owner = newOwner;
-    //     // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-    //     await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-    //     return oldOwner;
-    // }
-
-
-    /// Validation
-
-    async CheckEmailExist(ctx, email) {
-        let queryString = {};
-        queryString.selector = {};
-		queryString.selector.email_id = email;
-        const assetJSON = await this.GetQueryResultForQueryString(ctx, JSON.stringify(queryString));
-        return assetJSON && assetJSON.length > 0;
-    }
-
-    async CheckPublicIdPasswordExist(ctx, publicId, password) {
-        let queryString = {};
-        queryString.selector = {};
-		queryString.selector.public_id = publicId;
-		queryString.selector.password = password;
-        const assetJSON = await this.GetQueryResultForQueryString(ctx, JSON.stringify(queryString));
-        return assetJSON;
-    }
-
-    /// Query
-
-    async GetQueryResultForQueryString(ctx, queryString) {
-		let resultsIterator = await ctx.stub.getQueryResult(queryString);
-		let results = await this._GetAllResults(resultsIterator, false);
-        return results;
-	}
-
-    async GetUserById(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id);
-        if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`The asset ${id} does not exist`);
-        }
-        return JSON.parse(assetJSON.toString());
-    }
-
-
-    /// Utils
-
-    async _GetAllResults(iterator, isHistory) {
-		let allResults = [];
-		let res = await iterator.next();
-		while (!res.done) {
-			if (res.value && res.value.value.toString()) {
-				let jsonRes = {};
-				console.log(res.value.value.toString('utf8'));
-				if (isHistory && isHistory === true) {
-					jsonRes.TxId = res.value.txId;
-					jsonRes.Timestamp = res.value.timestamp;
-					try {
-						jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
-					} catch (err) {
-						console.log(err);
-						jsonRes.Value = res.value.value.toString('utf8');
-					}
-				} else {
-					jsonRes.Key = res.value.key;
-					try {
-						jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
-					} catch (err) {
-						console.log(err);
-						jsonRes.Record = res.value.value.toString('utf8');
-					}
-				}
-				allResults.push(jsonRes);
-			}
-			res = await iterator.next();
-		}
-		iterator.close();
-		return allResults;
-	} 
-
-
-
-    /// Temp  
-    async AddDummyUsers(ctx, count){
-        try {
-            for (let i = 1; i <= count; i++) {
-               let res =  await this.CreateUser(ctx, stringify(i), "user name" + i, "address " + i, "imageUrl_"+i, "mobileNo_"+i, "emailId_"+i, "publicId_"+i, true, "123456", "2023-11-09T10:37:26.031Z")
-            }
-            return "Success";
-        } catch (error) {
-            return Error(error);
-        }
-    }
+  }
 }
 
 module.exports = AssetTransfer;
--- skill-chain/skill-chain-server/app.js
+++ skill-chain/skill-chain-server/app.js
@@ -4,307 +4,261 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-'use strict'; 
- 
-const { Gateway, Wallets } = require('fabric-network');
-const FabricCAServices = require('fabric-ca-client');
-const path = require('path');
-const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('../../test-application/javascript/CAUtil.js');
-const { buildCCPOrg1, buildWallet } = require('../../test-application/javascript/AppUtil.js');
+"use strict";
+
+const { Gateway, Wallets } = require("fabric-network");
+const FabricCAServices = require("fabric-ca-client");
+const path = require("path");
+const {
+  buildCAClient,
+  registerAndEnrollUser,
+  enrollAdmin,
+} = require("../../test-application/javascript/CAUtil.js");
+const {
+  buildCCPOrg1,
+  buildWallet,
+} = require("../../test-application/javascript/AppUtil.js");
 const express = require("express");
 const bodyParser = require("body-parser");
-const { v4: uuidv4 } = require('uuid');
-const jwt = require('jsonwebtoken');
-const { log } = require('console');
-var lodash = require('lodash');
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+const { log } = require("console");
+var lodash = require("lodash");
 require("dotenv").config();
- 
-const channelName = process.env.CHANNEL_NAME || 'mychannel';
-const chaincodeName = process.env.CHAINCODE_NAME || 'basic';
- 
-const mspOrg1 = 'Org1MSP';
-const walletPath = path.join(__dirname, 'wallet');
-const org1UserId = 'javascriptAppUser';
+
+const channelName = process.env.CHANNEL_NAME || "mychannel";
+const chaincodeName = process.env.CHAINCODE_NAME || "basic";
+
+const mspOrg1 = "Org1MSP";
+const walletPath = path.join(__dirname, "wallet");
+const org1UserId = "javascriptAppUser";
 
 function prettyJSONString(inputString) {
-	return JSON.stringify(JSON.parse(inputString), null, 2);
+  return JSON.stringify(JSON.parse(inputString), null, 2);
 }
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 app.use(bodyParser.json());
 
 let _contract;
 
 async function connect() {
-	try {
-		const ccp = buildCCPOrg1();
-		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
-		const wallet = await buildWallet(Wallets, walletPath);
-		await enrollAdmin(caClient, wallet, mspOrg1);
-		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
-		const gateway = new Gateway();
-		try {
-			await gateway.connect(ccp, {
-				wallet,
-				identity: org1UserId,
-				discovery: { enabled: true, asLocalhost: true } 
-			});
+  try {
+    const ccp = buildCCPOrg1();
+    const caClient = buildCAClient(
+      FabricCAServices,
+      ccp,
+      "ca.org1.example.com"
+    );
+    const wallet = await buildWallet(Wallets, walletPath);
+    await enrollAdmin(caClient, wallet, mspOrg1);
+    await registerAndEnrollUser(
+      caClient,
+      wallet,
+      mspOrg1,
+      org1UserId,
+      "org1.department1"
+    );
+    const gateway = new Gateway();
+    try {
+      await gateway.connect(ccp, {
+        wallet,
+        identity: org1UserId,
+        discovery: { enabled: true, asLocalhost: true },
+      });
 
-			const network = await gateway.getNetwork(channelName);
+      const network = await gateway.getNetwork(channelName);
 
-			_contract = network.getContract(chaincodeName);
+      _contract = network.getContract(chaincodeName);
 
-			console.log("Connection Successfull!");
-
-		} finally {
-			// gateway.disconnect();
-		}
-	} catch (error) {
-		console.error(`******** FAILED to run the application: ${error}`);
-		process.exit(1);
-	}
+      console.log("Connection Successfull!");
+    } finally {
+      // gateway.disconnect();
+    }
+  } catch (error) {
+    console.error(`******** FAILED to run the application: ${error}`);
+    process.exit(1);
+  }
 }
 
 connect();
 
-app.get("/", async function (req, res)  {
-	let result = await _contract.evaluateTransaction('GetAllUsers');
-	return res.json(JSON.parse(prettyJSONString(result)));
+app.get("/", async function (req, res) {
+  let result = await _contract.evaluateTransaction("GetAllUsers");
+  return res.json(JSON.parse(prettyJSONString(result)));
 });
 
-async function checkIsEmailExist(email){
-	try{
-		let result = await _contract.evaluateTransaction('GetUserByEmail', email);
-		console.log(JSON.parse(prettyJSONString(result)));
-		return result != null;
-	} catch(e) {
-		console.log(e.message);
-		return false;
-	}
-}
+app.post("/signup", async function (req, res) {
+  var body = req.body;
+  try {
+    let result = await _contract.submitTransaction(
+      "CreateUser",
+      body.name,
+      body.address,
+      body.image_url,
+      body.mobile_no,
+      body.email_id,
+      generateUniqueId(),
+      body.status,
+      body.password,
+      Date.now()
+    );
 
-async function checkIsMobileExist(mobile){
-	try{
-		let result = await _contract.evaluateTransaction('GetUserByMobile', mobile);
-		console.log(JSON.parse(prettyJSONString(result)));
-		return result != null;
-	} catch(e) {
-		console.log(e.message);
-		return false;
-	}
-}
+    let jsonResult = JSON.parse(prettyJSONString(result));
+    let token = generateAccessToken({
+      id: jsonResult.id,
+      public_id: jsonResult.public_id,
+      email: jsonResult.email_id,
+    });
 
-async function checkIsUserNameExist(username){
-	try{
-		let result = await _contract.evaluateTransaction('GetUserByUserName', username);
-		console.log(JSON.parse(prettyJSONString(result)));
-		return result != null;
-	} catch(e) {
-		console.log(e.message);
-		return false;
-	} 
-}
-
-app.post("/validate_username", async function (req, res)  {
-	var body = req.body;
-	if(!await checkIsUserNameExist(body.username)){
-		return res.json({
-			"status": true,
-			"message": "Valid username",
-			"data": null,
-		})
-	} else {
-		return res.status(400).json({ status: false, message: 'Username already taken!', data: null });
-	}
-});
-
-app.post("/signup", async function (req, res)  {
-	var body = req.body;
-	if(await checkIsUserNameExist(body.user_name)){
-		return res.status(400).json({
-			"status": false,
-			"message": "Username already taken!",
-			"data": null
-		}); 
-	} 
-	if(await checkIsEmailExist(body.email_id)){
-		return res.status(400).json({
-			"status": false,
-			"message": "EmailId already exist!",
-			"data": null
-		});
-	}
-	if(await checkIsMobileExist(body.mobile_no)){
-		return res.status(400).json({
-			"status": false,
-			"message": "Mobile number already exist!",
-			"data": null
-		});
-	}
-	addTryBlock(
-		async ()=>{
-	    let result = await _contract.submitTransaction('CreateUser', uuidv4(), body.full_name, body.user_name, body.dob, body.mobile_no, body.email_id, body.password, Date.now());
-		let jsonResult = JSON.parse(prettyJSONString(result));
-		console.log(jsonResult.id);
-		let token = generateAccessToken({
-			id: jsonResult.id,
-			mobile: jsonResult.mobile_no,
-			email: jsonResult.email_id,
-		});
-	
-		return res.json({
-			"status": true,
-			"message": "Signup Successfull",
-			"data": {
-				"token": token,
-			}
-		});
-		}
-	);
-    
-});
-
-function addTryBlock(func){
-	try {
-		func();
-	} catch (e){
-		return res.json({
-			"status": false,
-			"message": e.message,
-			"data": null
-		});
-	}
-}
-
-function identifyInputType(input) {
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	const mobileRegex = /^[0-9]{10}$/;
-	const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-  
-	if (emailRegex.test(input)) {
-	  return 'Email';
-	} else if (mobileRegex.test(input)) {
-	  return 'Mobile';
-	} else if (usernameRegex.test(input)) {
-	  return 'Username';
-	} else {
-	  return 'Unknown';
-	}
+    return res.json({
+      status: true,
+      message: "Signup Successfull",
+      data: {
+        token: token,
+      },
+    });
+  } catch (e) {
+    return getError(e, res);
   }
-  
-
-app.post("/signin", async function (req, res)  {
-	var body = req.body;
-	var emailMobileName = body.email_mobile_name;
-	var password = body.password;
-
-	addTryBlock(
-		async ()=>{
-		let result = await _contract.evaluateTransaction('GetAllUsers');
-		var userList = JSON.parse(prettyJSONString(result));
-
-		var currentUser;
-		userList.forEach(element => {
-			if(identifyInputType(emailMobileName) == "Email"){
-				if(element.email_id == emailMobileName){
-					currentUser = element;
-				}
-			}
-			if(identifyInputType(emailMobileName) == "Mobile"){
-				if(element.mobile_no == emailMobileName){
-					currentUser = element;
-				}
-			} 
-			if(identifyInputType(emailMobileName) == "Username"){
-				if(element.user_name == emailMobileName){
-					currentUser = element;
-				}
-			}
-			if(identifyInputType(emailMobileName) == "Unknown"){
-				return res.status(403).json({status: false, message: 'Invalid Credentials!', data: null});
-			}
-		}); 
-
-		if(currentUser == null){
-			return res.status(400).json({status: false, message: 'Invalid Credentials!', data: null});
-		}   
-
-		if(currentUser.password == password){
-			let token = generateAccessToken({
-				id: currentUser.id,
-				mobile: currentUser.mobile_no,
-				email: currentUser.email_id,
-			});
-
-			return res.json({
-				"status": true,
-				"message": "Login Successfull",
-				"data": {
-					"token": token,
-				}
-			});
-		} else {
-			return res.status(400).json({ status: false, message: 'Invalid Credentials!', data: null });
-		}
-	});
 });
 
-function generateAccessToken(user) {
-	return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-}
+app.post("/login", async function (req, res) {
+  var body = req.body;
+
+  try {
+    let result = await _contract.submitTransaction(
+      "ValidateUser",
+      body.public_id,
+      body.password
+    );
+
+    let jsonResult = JSON.parse(prettyJSONString(result));
+    let token = generateAccessToken({
+      id: jsonResult.id,
+      public_id: jsonResult.public_id,
+      email: jsonResult.email_id,
+    });
+    return res.json({
+      status: true,
+      message: "Login Successfull",
+      data: {
+        token: token,
+      },
+    });
+  } catch (e) {
+    return getError(e, res);
+  }
+
+  // return res
+  //   .status(400)
+  //   .json({ status: false, message: "Username already taken!", data: null });
+});
+
+app.get("/user_from_public_id", async function (req, res) {
+  var body = req.body;
+  try {
+    let result = await _contract.evaluateTransaction("GetAllUsers");
+    var json = JSON.parse(prettyJSONString(result));
+    console.log(json);
+
+    const matchingRecords = json
+      .filter((item) => body.public_ids.includes(item.Key))
+      .map((item) => {
+        const { password, ...recordWithoutPassword } = item.Record;
+        return recordWithoutPassword;
+      });
+    // var jsonWithOutPassword = lodash.omit(json, "password");
+    return res.json({
+      status: matchingRecords.length > 0,
+      message: matchingRecords.length > 0 ? "User found" : "No users found!",
+      data: matchingRecords.length > 0 ? matchingRecords : null,
+    });
+  } catch (e) {
+    return getError(e, res);
+  }
+});
+
+app.post("/add_skill", async function (req, res) {
+  var body = req.body;
+  try {
+    let responseData = [];
+    await Promise.all(
+      body.users.map(async (element) => {
+        try {
+          let result = await _contract.submitTransaction(
+            "AddSkill",
+            element.public_id,
+            body.skill_id,
+            element.skill_doc_url,
+            element.institute_id
+          );
+          var json = JSON.parse(prettyJSONString(result));
+          responseData.push(true);
+        } catch (e) {
+          responseData.push(false);
+        }
+      })
+    );
+    console.log(responseData);
+    return res.json({
+      status: true,
+      message: "Add Skill Response",
+      data: responseData,
+    });
+  } catch (e) {
+    return getError(e, res);
+  }
+});
+
+// Utils
 
 function authenticateToken(req, res, next) {
-	const authHeader = req.headers['authorization']
-	const token = authHeader && authHeader.split(' ')[1]
-	if (token == null) return res.sendStatus(401)
-  
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-	  console.log(err)
-	  if (err) return res.sendStatus(403)
-	  req.user = user
-	  next()
-	})
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
-app.get("/profile", authenticateToken,  async function (req, res)  {
-	addTryBlock(
-		async ()=>{
-		let result = await _contract.evaluateTransaction('GetUserById', req.user.id);
-		var json = JSON.parse(prettyJSONString(result));
-		var jsonWithOutPassword = lodash.omit(json, "password");
-		return res.json({
-			"status": true,
-			"message": "",
-			"data": jsonWithOutPassword
-		});
-	});
-});
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+}
 
-app.post("/change_password", async function (req, res)  {
-	var body = req.body;
-	addTryBlock(
-		async ()=>{
-		let result = await _contract.submitTransaction('UpdatePassword', body.mobile_no, body.password, Date.now());
-		var json = JSON.parse(prettyJSONString(result));
-		var jsonWithOutPassword = lodash.omit(json, "password");
-		return res.json({
-			"status": true, 
-			"message": "Password Updated",
-			"data": jsonWithOutPassword
-		});
-	});
-}); 
+function getError(e, res) {
+  return res.json({
+    status: false,
+    message: e.message,
+    data: null,
+  });
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now().toString();
+  const randomNum = Math.floor(Math.random() * 1000000)
+    .toString()
+    .padStart(6, "0");
+  const uniqueId = timestamp + randomNum;
+  return uniqueId.slice(-12);
+}
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 3000;
+  port = 3000;
 }
 
 app.listen(port, function (req, res) {
-    console.log("server has started successfully!");
+  console.log("server has started successfully!");
 });
